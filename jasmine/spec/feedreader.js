03f5203a3c1e2983b902ba192958c38fb5a76c30
Sage Elliott <wildtroll@gmail.com>
2015-11-24 18:19:52 -0800

    Complete todo 1: - loop through each news feed itwm and check that url is defined - check that url does not equel 0

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('URLs are defined', function() {
+            for (var i = 0, len = allFeeds.length; i < len; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            }
+         });
 
 
         /* TODO: Write a test that loops through each feed
